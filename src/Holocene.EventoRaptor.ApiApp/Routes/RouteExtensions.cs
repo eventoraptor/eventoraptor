af02630228f2ead1742e46aa0a2bba527a6cd630
Justin Yoo <justin.yoojh@gmail.com>
2024-01-02 16:53:42 +1100

    Refactor weather forecast

---
+++ src/Holocene.EventoRaptor.ApiApp/Routes/RouteExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Holocene.EventoRaptor.ApiApp.Routes.Weather;
+
+namespace Holocene.EventoRaptor.ApiApp.Routes;
+
+public static class RouteExtensions
+{
+    public static RouteHandlerBuilder MapRoutes(this WebApplication app)
+    {
+        return app.GetWeatherForecast()
+                  ;
+    }
+}
---
+++ src/Holocene.EventoRaptor.ApiApp/Routes/Weather/GetWeatherForecast.cs
@@ -0,0 +1,30 @@
+﻿using Holocene.EventoRaptor.ApiApp.Models;
+
+namespace Holocene.EventoRaptor.ApiApp.Routes.Weather;
+
+public static partial class WeatherForecastExtensions
+{
+    private static string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public static RouteHandlerBuilder GetWeatherForecast(this WebApplication app)
+    {
+        return app.MapGet("/weatherforecast", async () =>
+        {
+            var forecast =  Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+
+            return await Task.FromResult(forecast).ConfigureAwait(false);
+        })
+        .WithName("GetWeatherForecast")
+        .WithOpenApi();
+    }
+}
